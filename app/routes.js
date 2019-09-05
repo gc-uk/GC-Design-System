75c05348a9e314bae95023a9aa1beb98573bc5a7
Andy Jones <hello@awj.digital>
2019-09-05 11:48:20 +0100

    updates

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,11 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+var componentsController = require('./controllers/componentController.js')
+
+
+
+router.get('/design-system/components/vertical-nav', componentsController.vertical_nav);
+
+
 module.exports = router
