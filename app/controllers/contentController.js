71ea4c07789c052c0285ba59df5a6d4c192de996
Cian Scollan <cscollan@gamblingcommission.gov.uk>
2019-11-04 12:04:36 +0000

    content revisions

--- app/controllers/contentController.js
+++ app/controllers/contentController.js
@@ -10,7 +10,6 @@ exports.index_get = function (req, res) {
     res.redirect('/content/content/introduction');
 }
 
-<<<<<<< HEAD
 exports.az_get = function (req, res) {
    
     var azActive = active;
@@ -72,39 +71,3 @@ exports.tools_get = function (req, res) {
     });
 }
 
-=======
-exports.content_get = function (req, res) {
-
-    console.log('contentPages content page')
-    var slug = req.params.id;
-    var content_page;
-    var list_of_pages;
-
-    console.log(slug)
-    Promise.all([
-            client.getEntries({
-                'content_type': 'contentPages',
-                'fields.slug': slug
-            }),
-            client.getEntries({
-                'content_type': 'contentPages',
-                order: 'fields.order'
-            })
-        ])
-        .then(([n, o]) => {
-            content_page = n,
-                list_of_pages = o
-
-            console.log(n)
-            res.render('content/content', {
-                content_page,
-                list_of_pages,
-                contentnavactive
-            });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-}
->>>>>>> d7ed0fb7ebda72813e8acfacb7f86981dc372f86
